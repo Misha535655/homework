e1ce7e53c977ff95cd516ae0344dfa8ee79fd8eb
Misha <visvan3333@gmail.com>
2020-02-06 21:34:48 +0300

    task1

---
+++ homework31-01/main.js
@@ -0,0 +1,13 @@
+// <<<<<<<<<<<<task2>>>>>>>>>>>>>
+var speed = +prompt("enter speed snail");
+if (speed > 0)
+{
+  alert("snail going forward");
+}
+else if (speed < 0){
+  alert (" snail going back");
+}
+else {
+  alert("snail standing still");
+}
+// <<<<<<<<<<<<<<<task3>>>>>>>>>>>>>
