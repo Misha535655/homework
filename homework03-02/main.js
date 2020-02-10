b78d316e54de6f309a6fa70c6580428336f7c260
Misha <visvan3333@gmail.com>
2020-02-10 16:23:07 +0300

    task3

--- homework03-02/main.js
+++ homework03-02/main.js
@@ -5,13 +5,19 @@
 //   console.log(a=i*i);
 // }else {};
 // };
-// <<<<<<<<<<<<<<<<<<<<<<task2>>>>>>>>>>>>>>>>>
-var b,c;
-
-for(let i = 0; i < 20; i++){
-
-if(i%2){
-  console.log ("#");
-}
-  else {console.log (".");}
+// // <<<<<<<<<<<<<<<<<<<<<<task2>>>>>>>>>>>>>>>>>
+// var b,c;
+//
+// for(let i = 0; i < 20; i++){
+//
+// if(i%2){
+//   console.log ("#");
+// }
+//   else {console.log (".");}
+// }
+// // <<<<<<<<<<<<<<<<<<<<<<task3>>>>>>>>>>>>>>>>>
+while(true){
+  let a = confirm("enter ok or confirm");
+  if (a == false ){}
+  else {break}
 }
