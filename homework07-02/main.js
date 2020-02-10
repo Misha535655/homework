9922449a58ba36ed5f2909e53e166a5653fb1814
Misha <visvan3333@gmail.com>
2020-02-10 03:37:42 +0300

    task2

--- homework07-02/main.js
+++ homework07-02/main.js
@@ -3,11 +3,23 @@ var person = {
   name: "Misha",
   sername: "Symonenko",
   age: "19",
-};
+  phone: "",};
 var key = prompt ("enter person");
 if (key in person){
-  alert (person[key]);
+  alert (person.key);
 } else {
-  person[key] = prompt("enter property name ");
+  person.key = prompt("enter property name ");
 }
-alert (person[key]);
+console.log (person[key]);
+// // <<<<<<<<<<<<<<<<<task2>>>>>>>>>>>>>>>
+var phone = {
+  brand: "",
+  model:"",
+  resolution:"",
+  color: "",
+};
+phone.brand = prompt("Enter brand phone");
+phone.model = prompt("Enter model");
+phone.resolution = prompt("Enter resolution");
+phone.color = prompt("Enter color");
+person.phone = phone;
